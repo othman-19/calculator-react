72e630485723f2c2013f501003304534a1a78856
othmane <othman-19@hotmail.fr>
2020-01-30 13:46:45 +0100

    customize Button style with props

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ buttonName, className }) => (
-  <button id={`btn-${buttonName}`} type="button" className={className}>{ buttonName }</button>
-);
+const Button = ({
+  buttonName,
+  className,
+  color,
+  wide,
+}) => {
+  if (wide) {
+    return (
+      <button id={`btn-${buttonName}`} type="button" className={`${className} wide`} style={{ background: color }}>{ buttonName }</button>
+    );
+  }
+  return (
+    <button id={`btn-${buttonName}`} type="button" className={`${className}`} style={{ background: color }}>{ buttonName }</button>
+  );
+};
+
+Button.defaultProps = {
+  color: 'orange',
+  wide: false,
+};
 
 Button.propTypes = {
   buttonName: PropTypes.string.isRequired,
   className: PropTypes.string.isRequired,
+  color: PropTypes.string,
+  wide: PropTypes.bool,
 };
 
 export default Button;
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -14,19 +14,30 @@ const ButtonPannel = () => {
   return (
     <div className="App">
       <div className="group" id="G1">
-        { groups[0].map(e => <Button className="Button" buttonName={e} key={e} />) }
+        { groups[0].filter(e => e !== groups[0][groups[0].length - 1])
+          .map(e => <Button color="rgb(163, 163, 172)" className="Button" buttonName={e} key={e} />) }
+        <Button className="Button" buttonName={groups[0][groups[0].length - 1]} key={groups[0][groups[0].length - 1]} />
       </div>
       <div className="group" id="G2">
-        { groups[1].map(e => <Button className="Button" buttonName={e} key={e} />) }
+        { groups[1].filter(e => e !== groups[1][groups[1].length - 1])
+          .map(e => <Button color="rgb(163, 163, 172)" className="Button" buttonName={e} key={e} />) }
+        <Button className="Button" buttonName={groups[1][groups[1].length - 1]} key={groups[1][groups[1].length - 1]} />
       </div>
       <div className="group" id="G3">
-        { groups[2].map(e => <Button className="Button" buttonName={e} key={e} />) }
+        { groups[2].filter(e => e !== groups[2][groups[2].length - 1])
+          .map(e => <Button color="rgb(163, 163, 172)" className="Button" buttonName={e} key={e} />) }
+        <Button className="Button" buttonName={groups[2][groups[2].length - 1]} key={groups[2][groups[2].length - 1]} />
       </div>
       <div className="group" id="G4">
-        { groups[3].map(e => <Button className="Button" buttonName={e} key={e} />) }
+        { groups[3].filter(e => e !== groups[3][groups[3].length - 1])
+          .map(e => <Button color="rgb(163, 163, 172)" className="Button" buttonName={e} key={e} />) }
+        <Button className="Button" buttonName={groups[3][groups[3].length - 1]} key={groups[3][groups[3].length - 1]} />
       </div>
       <div className="group" id="G5">
-        { groups[4].map(e => <Button className="Button" buttonName={e} key={e} />) }
+        <Button color="rgb(163, 163, 172)" className="Button" buttonName={groups[4][0]} wide key={groups[4][groups[4].length - 1]} />
+        { groups[4].filter(e => e !== groups[4][0] && e !== groups[4][groups[4].length - 1])
+          .map(e => <Button color="rgb(163, 163, 172)" className="Button" buttonName={e} key={e} />) }
+        <Button className="Button" buttonName={groups[4][groups[4].length - 1]} key={groups[4][groups[4].length - 1]} />
       </div>
     </div>
   );
