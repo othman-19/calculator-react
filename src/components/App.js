882bcacacccb28de228e12e859e25299a44dac8e
othmane <othman-19@hotmail.fr>
2020-01-30 21:12:12 +0100

    import calculate module in App component

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import '../index.css';
+import calculate from '../logic/calculate';
 
 class App extends React.Component {
   constructor(props) {
@@ -10,6 +11,7 @@ class App extends React.Component {
   }
 
   render() {
+    calculate();
     return (
       <div className="container">
         <header className="header">Easy Online Calculator</header>
