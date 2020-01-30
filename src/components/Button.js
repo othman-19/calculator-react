9f3738d4c42053dad3d2d2fbcceaa8aa76d44919
othmane <othman-19@hotmail.fr>
2020-01-30 17:59:28 +0100

    use ternary operator, delete not used attributes and props

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../index.css';
 
 const Button = ({
   buttonName,
-  className,
   color,
   wide,
-}) => {
-  if (wide) {
-    return (
-      <button id={`btn-${buttonName}`} type="button" className={`${className} wide`} style={{ background: color }}>{ buttonName }</button>
-    );
-  }
-  return (
-    <button id={`btn-${buttonName}`} type="button" className={`${className}`} style={{ background: color }}>{ buttonName }</button>
-  );
-};
-
+}) => (
+  wide ? <button id={`btn-${buttonName}`} type="button" className="Button wide" style={{ background: color }}>{ buttonName }</button>
+    : <button id={`btn-${buttonName}`} type="button" className="Button" style={{ background: color }}>{ buttonName }</button>
+);
 Button.defaultProps = {
   color: 'orange',
   wide: false,
@@ -24,7 +17,6 @@ Button.defaultProps = {
 
 Button.propTypes = {
   buttonName: PropTypes.string.isRequired,
-  className: PropTypes.string.isRequired,
   color: PropTypes.string,
   wide: PropTypes.bool,
 };
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -4,37 +4,36 @@ import '../index.css';
 
 const ButtonPannel = () => (
   <div className="App">
-    <div className="group" id="G1">
-      <Button color="rgb(163, 163, 172)" className="Button" buttonName="AC" key="AC" />
-      <Button color="rgb(163, 163, 172)" className="Button" buttonName="+/-" key="+/-" />
-      <Button color="rgb(163, 163, 172)" className="Button" buttonName="%" key="%" />
-      <Button className="Button" buttonName="÷" key="÷" />
+    <div className="group">
+      <Button color="rgb(163, 163, 172)" buttonName="AC" />
+      <Button color="rgb(163, 163, 172)" buttonName="+/-" />
+      <Button color="rgb(163, 163, 172)" buttonName="%" />
+      <Button buttonName="÷" />
     </div>
-    <div className="group" id="G2">
-      <Button color="rgb(163, 163, 172)" className="Button" buttonName="7" key="7" />
-      <Button color="rgb(163, 163, 172)" className="Button" buttonName="8" key="8" />
-      <Button color="rgb(163, 163, 172)" className="Button" buttonName="9" key="9" />
-      <Button className="Button" buttonName="x" key="x" />
+    <div className="group">
+      <Button color="rgb(163, 163, 172)" buttonName="7" />
+      <Button color="rgb(163, 163, 172)" buttonName="8" />
+      <Button color="rgb(163, 163, 172)" buttonName="9" />
+      <Button buttonName="x" />
     </div>
-    <div className="group" id="G3">
-      <Button color="rgb(163, 163, 172)" className="Button" buttonName="4" key="4" />
-      <Button color="rgb(163, 163, 172)" className="Button" buttonName="5" key="5" />
-      <Button color="rgb(163, 163, 172)" className="Button" buttonName="6" key="6" />
-      <Button className="Button" buttonName="-" key="-" />
+    <div className="group">
+      <Button color="rgb(163, 163, 172)" buttonName="4" />
+      <Button color="rgb(163, 163, 172)" buttonName="5" />
+      <Button color="rgb(163, 163, 172)" buttonName="6" />
+      <Button buttonName="-" />
     </div>
-    <div className="group" id="G4">
-      <Button color="rgb(163, 163, 172)" className="Button" buttonName="1" key="1" />
-      <Button color="rgb(163, 163, 172)" className="Button" buttonName="2" key="2" />
-      <Button color="rgb(163, 163, 172)" className="Button" buttonName="3" key="3" />
-      <Button className="Button" buttonName="+" key="+" />
+    <div className="group">
+      <Button color="rgb(163, 163, 172)" buttonName="1" />
+      <Button color="rgb(163, 163, 172)" buttonName="2" />
+      <Button color="rgb(163, 163, 172)" buttonName="3" />
+      <Button buttonName="+" />
     </div>
-    <div className="group" id="G5">
-      <Button color="rgb(163, 163, 172)" className="Button" buttonName="0" key="0" wide />
-      <Button color="rgb(163, 163, 172)" className="Button" buttonName="." key="." />
-      <Button className="Button" buttonName="=" key="=" />
+    <div className="group">
+      <Button color="rgb(163, 163, 172)" buttonName="0" wide />
+      <Button color="rgb(163, 163, 172)" buttonName="." />
+      <Button buttonName="=" />
     </div>
   </div>
 );
-
 
 export default ButtonPannel;
