61f4aa81bb38d7b6e5e0db764e639e0f7e8161ee
othmane <othman-19@hotmail.fr>
2020-01-30 21:07:54 +0100

    add the operate module

---
+++ src/logic/operate.js
@@ -0,0 +1,28 @@
+import Big from 'big.js';
+
+const operate = ((numberOne, numberTwo, operation) => {
+  try {
+    const num1 = Big(numberOne);
+    const num2 = Big(numberTwo);
+    if (operation === '+') {
+      return Big(num1).plus(Big(num2)).toString();
+    }
+    if (operation === '-') {
+      return Big(num1).minus(Big(num2)).toString();
+    }
+    if (operation === '*') {
+      return Big(num1).times(Big(num2)).toString();
+    }
+    if (operation === '%') {
+      return Big(num1).div(100).toString();
+    }
+    if (operation === '/' && num2 !== 0) {
+      return Big(num1).div(Big(num2)).toString();
+    }
+    return 'Error!';
+  } catch (e) {
+    return '0';
+  }
+})();
+
+export default operate;
